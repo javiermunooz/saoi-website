cd8f725eeb07f77c09827169bf7e01a5ac5c6b50
fjavier97vall <fjavier97vall@gmail.com>
2020-07-13 14:53:48 +0200

    Database & Contact form working

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,16 +1,18 @@
 <html>
 <body>
+HOLA
 
 <?php
 //Retrieved from HTML Form
-$name = $_POST["name"];
-$phone = $_POST["phone"];
-$email = $_POST["email"];
-$subject = $_POST["subject"];
-$message = $_POST["message"];
 
-$message = "From: " . $name . "\nEmail: " . $email . "\nphone: " . $phone . "\n" . $message
-$to = "fjavier97vall@gmail.com"
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$message = "From: " . $name . "\nEmail: " . $email . "\nphone: " . $phone . "\n" . $message;
+$to = "contact@saoi.tech";
 
 //Send email
 $retval = mail ($to,$subject,$message);
@@ -21,8 +23,10 @@ if( $retval == true ) {
 }else {
     echo "Message could not be sent...";
 }
+
+header('Location: ../../index.html');
 		 
 ?>
 
 </body>
-</html> 
+</html>
